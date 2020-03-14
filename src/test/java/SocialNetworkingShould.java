6e0bf1d4c92d30eed17ff1b28be0aa5e152f83a0
Miguel Ángel Viera <mangelviera93@gmail.com>
2020-03-14 01:36:37 +0100

    Initial commit

---
+++ src/test/java/SocialNetworkingShould.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SocialNetworkingShould {
+    @Test
+    public void
+    xxx() {
+        assertThat(true).isTrue();
+    }
+}
